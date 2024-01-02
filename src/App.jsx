afe43c4dc7659b6781fa720a0f46d643d208d65d
xtung6121 <xuantung6121@gmail.com>
2024-01-02 15:34:31 +0700

    Refactor Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,12 @@
-import Button from '@mui/material/Button'
-import AbcIcon from '@mui/icons-material/Abc'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
-import { useColorScheme } from '@mui/material/styles'
+import Board from './pages/Boards/_id'
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
 function App() {
-
   return (
     <>
-      <ModeToggle />
-      <hr />
-      <div>XuanTung</div>
-      <Button variant="text">Hello world</Button>
-      <Button variant="contained">Text</Button>
-      <Button variant="outlined">UI Button</Button>
-      <AbcIcon></AbcIcon>
-
-      <Typography variant="subtitle1">subtitle</Typography>
-      <br></br>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-
+      <Board/>
     </>
   )
 }
 
 export default App
+
---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,4 @@
+// Authentication SignIn-Signout
+// Authen 
+
+
